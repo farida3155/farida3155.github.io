89269fa3a58177f630bede970df78ab3ad28f499
farida3155 <faridahossam19@gmail.com>
2026-05-21 23:17:12 +0300

    Delete backend directory

--- backend/src/main/java/com/mello/repository/HabitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mello.repository;
-
-import com.mello.model.Habit;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface HabitRepository extends MongoRepository<Habit, String> {
-    List<Habit> findByUserId(String userId);
-}
--- backend/src/main/java/com/mello/repository/RecommendationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mello.repository;
-
-import com.mello.model.RecommendationEntry;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface RecommendationRepository extends MongoRepository<RecommendationEntry, String> {
-    List<RecommendationEntry> findByUserId(String userId);
-}
